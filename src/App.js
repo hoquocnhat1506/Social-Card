c38a2b3121acc8301dbb3cfe4b7703692e20f247
hoquocnhat1506 <89431816+hoquocnhat1506@users.noreply.github.com>
2023-08-15 08:47:15 +0700

    Done UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Button from "./Components/Custom/Button";
+import PageNotFound from "./Components/NotFound/PageNotFound";
+import Item from "./Components/Item/Item";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content">
+        <div className="title">LIST SOCIAL CARD</div>
+        <div className="custom">
+          <Button />
+        </div>
+        <div className="not-found">
+          <Item />
+          <PageNotFound />
+        </div>
+      </div>
     </div>
   );
 }
